5dae576e53e4bc5d7a71216ac254344a41febe50
deanta-vinothkumar <vkumar@deantaglobal.com>
2021-12-15 20:15:33 +0530

    Form and action is created

--- src/Controller/ConfigurationController.php
+++ src/Controller/ConfigurationController.php
@@ -3,18 +3,36 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+
+
 
 class ConfigurationController extends AbstractController
 {
     /**
      * @Route("/", name="configuration")
      */
-    public function index(): Response
+    public function index()
     {
         return $this->render('configuration/index.html.twig', [
             'controller_name' => 'ConfigurationController',
         ]);
     }
+
+    /**
+     * @Route("/call/upload/spreadsheet", name="uploadSpreadSheet", methods={ "POST" })
+     * 
+     * @param Request $request
+     * 
+     * @return Response
+     */
+    public function uploadSpreadSheet(Request $request)
+    {   
+        //print_r($_FILES['test']);
+        return new JsonResponse($_FILES['expense_sheet']);
+        
+    }
 }
+
